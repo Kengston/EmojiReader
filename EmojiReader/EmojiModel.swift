3a8fec44aadbddba5d77850a6571f47f4967eec8
Danil Lysikov <danildc@mail.ru>
2020-05-08 18:20:01 +0700

    Release

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Данил Лысиков on 08.05.2020.
+//  Copyright © 2020 Данил Лысиков. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
---
+++ EmojiTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by Данил Лысиков on 08.05.2020.
+//  Copyright © 2020 Данил Лысиков. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func set(object: Emoji) {
+        self.emojiLabel.text = object.emoji
+        self.nameLabel.text = object.name
+        self.descriptionLabel.text = object.description
+    }
+
+}
